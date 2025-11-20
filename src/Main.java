d61159dd9bef10e3a7b1ef24e27f1d83c7b38d4b
Matheus Cantarutti <matheuscantarutti@Matheuss-MacBook-Air.local>
2025-11-20 16:47:34 -0300

    extention reader and class instantiation

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
 import PurchaseOrder.model.InternationalOrder;
 import PurchaseOrder.model.NationalOrder;
 import PurchaseOrder.model.OrderProcessor;
+import StrategyReport.Reports.CSVReport;
+import StrategyReport.Reports.JsonReport;
+import StrategyReport.Reports.XMLReport;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         IO.println("EXERCISE 2");
         IO.println("Strategy Report System");
 
+        String fileName = "dados.csv";
+
+        // file format etx
+        String etx = fileName.substring(fileName.length() - 3 );
+
+        switch (etx) {
+
+            case "csv":
+                // instantiate java class
+                new CSVReport();
+                break;
+            case "json":
+                // instantiate java class
+                new JsonReport();
+                break;
+            case "xml":
+                new XMLReport();
+                // instantiate java class
+                break;
+
+            default:
+                IO.println("etx not supported");
+                break;
+        }
+
+        /*
         IO.println("================================");
         IO.println("EXERCISE 1");
         IO.println("Purchased Order System");
@@ -20,5 +49,7 @@ public class Main {
         System.out.println(
                 "Total Ordered: " + processor.calculateTotal()
         );
+
+        */
     }
 }
