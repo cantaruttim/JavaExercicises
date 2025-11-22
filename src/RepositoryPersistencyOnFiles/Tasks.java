1ded6d3390f19246fcc56e471e4720012ebc7729
cantaruttim <cantaruttim@outlook.com>
2025-11-21 21:44:24 -0300

    tasks class

---
+++ src/RepositoryPersistencyOnFiles/Tasks.java
@@ -0,0 +1,50 @@
+package RepositoryPersistencyOnFiles;
+
+public class Tasks {
+
+    private int id;
+    private String description;
+    private boolean concluded;
+
+    public Tasks(int id, String description, boolean concluded) {
+        this.id = id;
+        this.description = description;
+        this.concluded = concluded;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isConcluded() {
+        return concluded;
+    }
+
+    public void setConcluded(boolean concluded) {
+        this.concluded = concluded;
+    }
+
+    @Override
+    public String toString() {
+        return id + ';' + description + ';' + concluded;
+    }
+
+    public static Tasks fromString(String rows) {
+        String[] parts = rows.split(";");
+        return new Tasks(
+                Integer.parseInt(parts[0]), // return Id
+                parts[1], // return description
+                Boolean.parseBoolean(parts[2]) // return concluded
+        );
+    }
+
+
+}
