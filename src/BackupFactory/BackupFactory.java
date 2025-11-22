c4791b3c50284b42d7247eced7fe23b6f357a55a
cantaruttim <cantaruttim@outlook.com>
2025-11-21 21:27:37 -0300

    buildBackUpFactory

---
+++ src/BackupFactory/BackupFactory.java
@@ -0,0 +1,15 @@
+package BackupFactory;
+
+public abstract class BackupFactory implements BackupService {
+
+    private BackupService buildBackUpFactory;
+
+    public BackupService getBuildBackUpFactory(){
+        return buildBackUpFactory;
+    }
+
+    public BackupService setBuildBackUpFactory(){
+        return this.buildBackUpFactory = buildBackUpFactory;
+    }
+
+}
